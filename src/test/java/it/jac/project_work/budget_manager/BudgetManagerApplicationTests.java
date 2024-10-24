c86c080c3d4251535af217966a327ef364797411
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-10-24 16:50:49 +0200

    spring initialization

---
+++ src/test/java/it/jac/project_work/budget_manager/BudgetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.jac.project_work.budget_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
