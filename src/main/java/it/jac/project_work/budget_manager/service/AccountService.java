f53201a96a4ea5650c6c43f59298150fcdd134e1
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-10-26 12:59:52 +0200

    new(AccountService)

---
+++ src/main/java/it/jac/project_work/budget_manager/service/AccountService.java
@@ -0,0 +1,28 @@
+package it.jac.project_work.budget_manager.service;
+
+
+import it.jac.project_work.budget_manager.dto.AccountInDTO;
+import it.jac.project_work.budget_manager.entity.Account;
+import it.jac.project_work.budget_manager.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+public class AccountService {
+    @Autowired
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+
+
+
+}
