230eb312632c77a757a93ced4c145ed2e6743f62
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-10-26 12:58:39 +0200

    new(accountController)

---
+++ src/main/java/it/jac/project_work/budget_manager/controller/AccountController.java
@@ -0,0 +1,13 @@
+package it.jac.project_work.budget_manager.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+
+
+}
