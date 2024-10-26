f1a4491eb77aa30523a327fc15ce7b3f131eccb4
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-10-26 12:59:27 +0200

    new(accountOutDTO for responses, build method to fix )

---
+++ src/main/java/it/jac/project_work/budget_manager/dto/AccountOutDTO.java
@@ -0,0 +1,106 @@
+package it.jac.project_work.budget_manager.dto;
+
+import it.jac.project_work.budget_manager.entity.Account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountOutDTO {
+
+    private String name;
+    private String surname;
+    private String email;
+    private String birthdate;
+    private String image;
+    private List<AccountOutDTO> children;
+    private AccountOutDTO parent;
+
+
+
+    public AccountOutDTO(){
+
+    }
+
+    public AccountOutDTO(String name, String surname, String email, String birthdate, String image, List<AccountOutDTO> children, AccountOutDTO parent) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.image = image;
+        this.children = children;
+        this.parent = parent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public List<AccountOutDTO> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<AccountOutDTO> children) {
+        this.children = children;
+    }
+
+    public AccountOutDTO getParent() {
+        return parent;
+    }
+
+    public void setParent(AccountOutDTO parent) {
+        this.parent = parent;
+    }
+
+    public static AccountOutDTO build(Account entity) {
+        AccountOutDTO dto = new AccountOutDTO();
+        dto.setName(entity.getName());
+        dto.setSurname(entity.getSurname());
+        dto.setEmail(entity.getEmail());
+        dto.setBirthdate(entity.getBithdate() != null ? entity.getBithdate().toString() : null);
+        dto.setImage(entity.getImage());
+        dto.setParent(entity.getParent() != null ? AccountOutDTO.build(entity.getParent()) : null);
+
+        List<AccountOutDTO> childrenList = new ArrayList<>();
+        entity.getChildren().forEach(ch -> childrenList.add(AccountOutDTO.build(ch)));
+        dto.setChildren(childrenList);
+
+        return dto;
+    }
+
+}
