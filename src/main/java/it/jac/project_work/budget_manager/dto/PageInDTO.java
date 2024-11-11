0a3a4d81c066006d20000c5fbb5c7cc87407c7ae
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-11 12:13:35 +0100

    fix(added filter directions)

--- src/main/java/it/jac/project_work/budget_manager/dto/PageInDTO.java
+++ src/main/java/it/jac/project_work/budget_manager/dto/PageInDTO.java
@@ -4,14 +4,24 @@ public class PageInDTO {
     private String orderBy;
     private Integer page;
     private Integer size;
+    private String orderDirection;
 
     public PageInDTO() {
     }
 
-    public PageInDTO(String orderBy, Integer page, Integer size) {
+    public PageInDTO(String orderBy, Integer page, Integer size, String orderDirection) {
         this.orderBy = orderBy;
         this.page = page;
         this.size = size;
+        this.orderDirection = orderDirection;
+    }
+
+    public String getOrderDirection() {
+        return orderDirection;
+    }
+
+    public void setOrderDirection(String orderDirection) {
+        this.orderDirection = orderDirection;
     }
 
     public String getOrderBy() {
