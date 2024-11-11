1f5685f48febc007f31806b0fd20280f430f5d66
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-11 12:13:20 +0100

    new(all incomes with pagination method)

--- src/main/java/it/jac/project_work/budget_manager/repository/IncomeRepository.java
+++ src/main/java/it/jac/project_work/budget_manager/repository/IncomeRepository.java
@@ -3,12 +3,19 @@ package it.jac.project_work.budget_manager.repository;
 import it.jac.project_work.budget_manager.dto.IncomeOutDTO;
 import it.jac.project_work.budget_manager.entity.Account;
 import it.jac.project_work.budget_manager.entity.Income;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import org.springframework.data.domain.Pageable;
 import java.sql.Date;
 import java.util.List;
 
 public interface IncomeRepository extends JpaRepository<Income, Long> {
     List<Income> findByAccountAndDateGreaterThanEqualOrderByDateDesc(Account account, Date date);
     List<Income> findByAccount(Account account);
+
+    @Query("SELECT i FROM Income i WHERE i.account.id = :id")
+    Page<Income> findAllWithPagination(@Param("id") Long id, Pageable pageable);
 }
