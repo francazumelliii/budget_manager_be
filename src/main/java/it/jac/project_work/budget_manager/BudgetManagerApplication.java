a98a0c30e5b5ea95a39d3870b1a2952c4e6a376e
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-20 19:51:35 +0100

    fix(scheduled task run on startup)

--- src/main/java/it/jac/project_work/budget_manager/BudgetManagerApplication.java
+++ src/main/java/it/jac/project_work/budget_manager/BudgetManagerApplication.java
@@ -1,13 +1,18 @@
 package it.jac.project_work.budget_manager;
 
+import it.jac.project_work.budget_manager.tasks.ScheduledTasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BudgetManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BudgetManagerApplication.class, args);
-    }
+        ConfigurableApplicationContext context = SpringApplication.run(BudgetManagerApplication.class, args);
 
+        ScheduledTasks scheduledTasks = context.getBean(ScheduledTasks.class);
+
+        scheduledTasks.runDailyDateUpdates();
+    }
 }
