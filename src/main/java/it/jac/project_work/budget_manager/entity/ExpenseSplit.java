e47d25ab08711691c998d408363e0d49706f023c
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-28 16:58:20 +0100

    new(expenseSplit entity)

---
+++ src/main/java/it/jac/project_work/budget_manager/entity/ExpenseSplit.java
@@ -0,0 +1,56 @@
+package it.jac.project_work.budget_manager.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "expense_split")
+public class ExpenseSplit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "expense_id", nullable = false)
+    private Expense expense;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @Column(name = "amount", nullable = false)
+    private Double amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Expense getExpense() {
+        return expense;
+    }
+
+    public void setExpense(Expense expense) {
+        this.expense = expense;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
