29fa151b99ecc5f787ee81f76d7497cacd3278a6
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-28 16:59:48 +0100

    update(added splitAmount parameter)

--- src/main/java/it/jac/project_work/budget_manager/dto/SimpleAccountOutDTO.java
+++ src/main/java/it/jac/project_work/budget_manager/dto/SimpleAccountOutDTO.java
@@ -9,15 +9,20 @@ public class SimpleAccountOutDTO {
     private String email;
     private String image;
 
+    private Double splitAmount;
+
     private SimpleAccountOutDTO(){
     }
 
-    private SimpleAccountOutDTO(String name, String surname, String email, String image) {
+    public SimpleAccountOutDTO(String name, String surname, String email, String image, Double splitAmount) {
         this.name = name;
         this.surname = surname;
         this.email = email;
+        this.splitAmount = splitAmount;
         this.image = image;
     }
+
+
 
     public static SimpleAccountOutDTO build(Account entity){
         SimpleAccountOutDTO dto = new SimpleAccountOutDTO();
@@ -26,6 +31,23 @@ public class SimpleAccountOutDTO {
         dto.setEmail(entity.getEmail());
         dto.setImage(entity.getImage());
         return dto;
+    }
+    public static SimpleAccountOutDTO build(Account account, Double splitAmount) {
+        SimpleAccountOutDTO dto = new SimpleAccountOutDTO();
+        dto.setName(account.getName());
+        dto.setEmail(account.getEmail());
+        dto.setSplitAmount(splitAmount);
+
+        return dto;
+    }
+
+
+    public Double getSplitAmount() {
+        return splitAmount;
+    }
+
+    public void setSplitAmount(Double splitAmount) {
+        this.splitAmount = splitAmount;
     }
 
     public String getName() {
