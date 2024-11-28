0b901f44a35798464b9a544b39454ce10197d06b
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-28 16:58:26 +0100

    new(expenseSplitRepository)

---
+++ src/main/java/it/jac/project_work/budget_manager/repository/ExpenseSplitRepository.java
@@ -0,0 +1,15 @@
+package it.jac.project_work.budget_manager.repository;
+
+import it.jac.project_work.budget_manager.entity.Account;
+import it.jac.project_work.budget_manager.entity.Expense;
+import it.jac.project_work.budget_manager.entity.ExpenseSplit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ExpenseSplitRepository extends JpaRepository<ExpenseSplit, Long> {
+    public ExpenseSplit findExpenseSplitByAccountAndExpense(Account account, Expense expense);
+    public List<ExpenseSplit> findExpenseSplitByExpense(Expense expense);
+}
