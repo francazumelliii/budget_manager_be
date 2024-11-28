5e7be4873966cbf0b0e8967a7e1aadbca5a074ec
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-11-28 16:59:37 +0100

    new(findByProjectId query)

--- src/main/java/it/jac/project_work/budget_manager/repository/ShareRepository.java
+++ src/main/java/it/jac/project_work/budget_manager/repository/ShareRepository.java
@@ -4,6 +4,10 @@ import it.jac.project_work.budget_manager.entity.Share;
 import it.jac.project_work.budget_manager.entity.ShareId;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ShareRepository extends CrudRepository<Share, ShareId> {
     void deleteById(ShareId id);
+
+    List<Share> findByProjectId(Integer projectId);
 }
