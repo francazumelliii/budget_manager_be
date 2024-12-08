f5f5e1f3687ebbc54979269b43a9f4463b9a3d63
Franca Zumelli <kqp6hsjtp6@privaterelay.appleid.com>
2024-12-08 11:45:26 +0100

    fix(findProjectById with share JOIN column)

--- src/main/java/it/jac/project_work/budget_manager/repository/ShareRepository.java
+++ src/main/java/it/jac/project_work/budget_manager/repository/ShareRepository.java
@@ -1,7 +1,9 @@
 package it.jac.project_work.budget_manager.repository;
 
+import it.jac.project_work.budget_manager.entity.Account;
 import it.jac.project_work.budget_manager.entity.Share;
 import it.jac.project_work.budget_manager.entity.ShareId;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -10,4 +12,8 @@ public interface ShareRepository extends CrudRepository<Share, ShareId> {
     void deleteById(ShareId id);
 
     List<Share> findByProjectId(Integer projectId);
+    @Query("SELECT s From Share s INNER JOIN Project p ON s.project = p WHERE p.account = :account")
+    List<Share> findAllByAccount(Account account);
+
+
 }
